1f1fbd40025c67fb003c7dc8e383a4c5b15f7c12
mpl <mathieu.lonjaret@gmail.com>
2016-02-03 20:00:21 +0100

    readerutil: more types from Camlistore
    From camlistore.org/pkg/types at rev 34ad44385b68953473522a7f002d5db01e2b7e3c
    
    Change-Id: I70dad9a4465250c3ba13cd7ddb5af5d7c19a9718

--- readerutil/readerutil.go
+++ readerutil/readerutil.go
@@ -17,7 +17,10 @@ limitations under the License.
 // Package readerutil contains io.Reader types.
 package readerutil // import "go4.org/readerutil"
 
-import "io"
+import (
+	"expvar"
+	"io"
+)
 
 // A SizeReaderAt is a ReaderAt with a Size method.
 //
@@ -25,4 +28,57 @@ import "io"
 type SizeReaderAt interface {
 	Size() int64
 	io.ReaderAt
+}
+
+// A ReadSeekCloser can Read, Seek, and Close.
+type ReadSeekCloser interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+}
+
+type ReaderAtCloser interface {
+	io.ReaderAt
+	io.Closer
+}
+
+// TODO(wathiede): make sure all the stat readers work with code that
+// type asserts ReadFrom/WriteTo.
+
+type varStatReader struct {
+	*expvar.Int
+	r io.Reader
+}
+
+// NewReaderStats returns an io.Reader that will have the number of bytes
+// read from r added to v.
+func NewStatsReader(v *expvar.Int, r io.Reader) io.Reader {
+	return &varStatReader{v, r}
+}
+
+func (v *varStatReader) Read(p []byte) (int, error) {
+	n, err := v.r.Read(p)
+	v.Int.Add(int64(n))
+	return n, err
+}
+
+type varStatReadSeeker struct {
+	*expvar.Int
+	rs io.ReadSeeker
+}
+
+// NewReaderStats returns an io.ReadSeeker that will have the number of bytes
+// read from rs added to v.
+func NewStatsReadSeeker(v *expvar.Int, rs io.ReadSeeker) io.ReadSeeker {
+	return &varStatReadSeeker{v, rs}
+}
+
+func (v *varStatReadSeeker) Read(p []byte) (int, error) {
+	n, err := v.rs.Read(p)
+	v.Int.Add(int64(n))
+	return n, err
+}
+
+func (v *varStatReadSeeker) Seek(offset int64, whence int) (int64, error) {
+	return v.rs.Seek(offset, whence)
 }
